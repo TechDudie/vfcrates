f8f659a05a2c19e023452adaf85c09745c21cb99
TechDudie <73961295+TechDudie@users.noreply.github.com>
2022-03-12 20:20:00 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -17,30 +17,32 @@ minetest.register_node("vfcrates:key_opener", {
 
 function register_key(name, loot, texture)
   local img = "vfcrates_default_key.png"
-  if texture == nil then
-    img = "vfcrates_" .. name .. "_key.png"
+  img = "vfcrates_" .. name .. "_key.png"
+  if texture ~= nil then
+    if texture:sub(1, 1) == "#" then
+      img = "vfcrates_default_key.png^[colorize:" .. texture .. ":255"
+    end
   end
-  if texture:sub(1, 1) == "#" then
-    img = "vfcrates_default_key.png^[colorize:" .. texture .. ":255"
-  end
-  minetest.register_craftitem("vfcrates:" .. name .. "_key", {
+  minetest.register_tool("vfcrates:" .. name .. "_key", {
     description = upper(name) .. " Key",
     inventory_image = "vfcrates_" .. name .. "_key.png",
     on_use = function(itemstack, user, pointed_thing)
       if minetest.get_node(pointed_thing.under).name == "vfcrates:key_opener" then
         local player_name = user:get_player_name()
-        local returned = take_item(itemstack)
-        returned = set_name(get_name(returned) .. "\n" .. minetest.colorize("#00FF00", upper(name) .. " Crate Loot"))
         local drop = loot[math.random(#loot)]
-        minetest.chat_send_all(playername .. " won a " .. drop .. " from a " .. upper(name) .. " Crate!")
-        user:get_inventory():add_item("main", get_name(returned) .. " " .. get_count(returned))
-        return drop
+        local returned = ItemStack(drop)
+        local loot_name = minetest.registered_items[returned:get_name()].description
+        local loot_meta = returned:get_meta()
+        minetest.chat_send_all(player_name .. " won a " .. loot_name .. " from a " .. upper(name) .. " Crate!")
+        loot_meta:set_string("description", loot_name .. "\n" .. minetest.colorize("#00FF00", upper(name) .. " Crate Loot"))
+        return returned
       end
     end
   })
 end
 
-loot = {"jumpdrive:engine 16", "jumpdrive:fleet_controller 2", "jumpdrive:backbone 99", "xtraores:helmet_chromium", "xtraores:chestplate_chromium", "xtraores:leggings_chromium", "xtraores:boots_chromium", "xtraores:pickaxe_chromium"}
+--loot = {"jumpdrive:engine 16", "jumpdrive:fleet_controller 2", "jumpdrive:backbone 99", "xtraores:helmet_chromium", "xtraores:chestplate_chromium", "xtraores:leggings_chromium", "xtraores:boots_chromium", "xtraores:pickaxe_chromium"}
+loot = {"jumpdrive:engine 16", "jumpdrive:fleet_controller 2", "jumpdrive:backbone 99"}
 register_key("silver", loot)
 register_key("gold", loot)
 register_key("vfium", loot)
